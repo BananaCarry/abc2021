83db2a2583eb473a3465a0e4e889a3162676494a
Emilian <emilian@osut.ro>
2021-03-08 00:11:46 +0200

    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ AOS.init();
 $(".buton-text").click(function () {
 	$(".sectiune-selectata").removeClass("sectiune-selectata");
 	$(this).addClass("sectiune-selectata");
-	if ($("#text-" + $(this).attr("id")).is(":visible")) return;
+	if ($("#text-" + $(this).attr("id")).is(":visible")) return $(".texte").slideUp();
 	$(".texte").slideUp();
 	$("#text-" + $(this).attr("id")).slideDown();
 });
